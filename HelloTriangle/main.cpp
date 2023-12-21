2d151f56096cc2b925bbb0345f88fda3d3eb98a9
NineTailSecurity <ninetailsoftware@gmail.com>
2023-12-21 01:07:19 -0500

    Instance Creation next up Validation Layers

--- HelloTriangle/main.cpp
+++ HelloTriangle/main.cpp
@@ -19,6 +19,7 @@ public:
 
 private:
 	GLFWwindow* window;
+	VkInstance instance;
 
     	void initWindow() {
 		glfwInit();
@@ -30,7 +31,7 @@ private:
 	}
 
     	void initVulkan() {
-
+		createInstance();
     	}
 
     	void mainLoop() {
@@ -40,10 +41,41 @@ private:
     	}
 
     	void cleanup() {
+		vkDestroyInstance(instance, nullptr);
+
 		glfwDestroyWindow(window);
 
     		glfwTerminate();
     	}
+
+	void createInstance() {
+                VkApplicationInfo appInfo{};
+                appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+                appInfo.pApplicationName = "Hello Triangle";
+                appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+                appInfo.pEngineName = "No Engine";
+                appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+                appInfo.apiVersion = VK_API_VERSION_1_0;
+
+                VkInstanceCreateInfo createInfo{};
+                createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+                createInfo.pApplicationInfo = &appInfo;
+
+                uint32_t glfwExtensionCount = 0;
+                const char** glfwExtensions;
+                glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+                createInfo.enabledExtensionCount = glfwExtensionCount;
+                createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+                createInfo.enabledLayerCount = 0;
+
+                if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+                        throw std::runtime_error("failed to create instance!");
+                }
+
+        }
+
 };
 
 int main() {
